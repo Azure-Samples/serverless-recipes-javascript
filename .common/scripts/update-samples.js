7f29bca41f0af80e4a00036b2d4d8b1bbde6d36a
sinedied <noda@free.fr>
2025-01-29 18:38:09 +0100

    feat: implement samples update

--- .common/scripts/update-samples.js
+++ .common/scripts/update-samples.js
@@ -1,43 +1,110 @@
 #!/usr/bin/env node
 /*
- * Updates common parts of samples.
+ * Automated update of one or all samples.
+ * If no sample name is provided, all samples will be updated. 
  * Usage: update-sample.js [sample-name]
  */
 
+import process from 'node:process';
 import path from 'node:path';
 import { existsSync, mkdirSync, readdirSync, readFileSync, writeFileSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
+import { execSync } from 'node:child_process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const sampleDir = path.join(__dirname, '..', '..', 'samples');
+const rootDir = path.join(__dirname, '..', '..');
+const sampleDir = path.join(rootDir, 'samples');
+const templatesDir = path.join(__dirname, '..', 'templates');
 const infraDir = path.join(__dirname, '..', 'infra');
+const referencePackageJson = JSON.parse(readFileSync(path.join(rootDir, 'package.json')));
+const dependencies = new Map([
+  ...Object.entries(referencePackageJson.dependencies),
+  ...Object.entries(referencePackageJson.devDependencies)
+]);
 
-// Update samples infrastructure
-for (const dirent of readdirSync(sampleDir, { withFileTypes: true })) {
-  if (dirent.isDirectory()) {
-    const samplePath = path.join(sampleDir, dirent.name);
-    const sampleInfraPath = path.join(samplePath, 'infra');
+const filesToUpdate = [
+  'tsconfig.json',
+  'update-local-settings.mjs'
+];
 
-    if (!existsSync(sampleInfraPath)) {
-      mkdirSync(sampleInfraPath, { recursive: true });
-    }
+function listSamples() {
+  return readdirSync(sampleDir, { withFileTypes: true })
+    .filter((entry) => entry.isDirectory())
+    .map((entry) => entry.name);
+}
 
-    for (const file of readdirSync(infraDir)) {
-      const sourceFile = path.join(infraDir, file);
-      const destinationFile = path.join(sampleInfraPath, file);
+function updateInfra(samplePath) {
+  const sampleInfraPath = path.join(samplePath, 'infra');
 
-      if (file !== 'services.json') {
-        writeFileSync(destinationFile, readFileSync(sourceFile));
-      }
+  if (!existsSync(sampleInfraPath)) {
+    mkdirSync(sampleInfraPath, { recursive: true });
+  }
+
+  for (const file of readdirSync(infraDir)) {
+    const sourceFile = path.join(infraDir, file);
+    const destinationFile = path.join(sampleInfraPath, file);
+
+    if (file !== 'services.json') {
+      writeFileSync(destinationFile, readFileSync(sourceFile));
     }
   }
 }
 
-// TODO:
-// - copy .editorconfig
-// - update package.json
-// - run `npm update` in each sample
+function updateDependenciesVersions(samplePath) {
+  const packageJsonPath = path.join(samplePath, 'package.json');
+  const packageJson = JSON.parse(readFileSync(packageJsonPath));
+  let changes = 0;
 
-// - run prettier (xo linter?) in each sample
+  const updatePackageSection = (section) => {
+    for (const packageName of Object.keys(section || {})) {
+      const version = dependencies.get(packageName);
+      if (version && section[packageName] !== version) {
+        section[packageName] = dependencies.get(packageName);
+        changes++;
+      }
+    }
+  }
+
+  updatePackageSection(packageJson.dependencies);
+  updatePackageSection(packageJson.devDependencies);
+
+  if (changes > 0) {
+    writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+    console.log(`Updated ${changes} dependencies`);
+    console.log('Running `npm install`...');
+    execSync('npm install', { cwd: samplePath, stdio: 'inherit' });
+  } else {
+    console.log('Running `npm update`...');
+    execSync('npm update', { cwd: samplePath, stdio: 'inherit' });
+  }
+}
+
+function lintAndFormat(samplePath) {
+  console.log('Running `npm run lint:fix`...');
+  execSync(`npm run lint:fix -- "${samplePath}"`, { cwd: rootDir, stdio: 'inherit' });
+}
+
+function main() {
+  if (process.argv.length > 3) {
+    console.log('Usage: node update-sample.js [sample-name]');
+    process.exit(1);
+  }
+
+  const samples = process.argv.length === 3 ? [process.argv[2]] : listSamples();
+  for (const sample of samples) {
+    console.log(`Updating sample "${sample}"...`);
+    const samplePath = path.join(sampleDir, sample);
+    updateInfra(samplePath);
+    updateDependenciesVersions(samplePath);
+    lintAndFormat(samplePath);
+  }
+
+  console.log(`Updated ${samples.length} samples.`);
+}
+
+main();
+
+// TODO:
+// update files from common or template name
